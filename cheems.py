9e8e407729f76e1e13fae4129a7ce4b4ac215637
cheemsburger <67502657+cheemsburger@users.noreply.github.com>
2020-06-27 22:53:10 +0500

    Updated code
    Now can convert normal language to CheemScrimpt 
    m goes brrrrrrrrrrrrr

--- cheems.py
+++ cheems.py
@@ -1,2 +1,13 @@
-def primt(obj):
-    print(obj)
+import random
+def primt(arg):
+    newString=""
+    if(type(arg) is str):
+        word_list = arg.split()
+        for word in word_list:
+            limit=len(word)
+            random_position=random.randint(1, limit-1)
+            newWord=word[:random_position] + "m" + word[random_position:] + " "
+            newString+=newWord
+        print(newString)
+    else:
+        print(arg)
